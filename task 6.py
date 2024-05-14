45449338f4f185b700611e684ad4f9dc24a20cbe
mandellq <167519564+mandellq@users.noreply.github.com>
2024-05-14 12:42:00 +0300

    Add files via upload

---
+++ task 6.py
@@ -0,0 +1,98 @@
+# if_else
+# if если True
+# is_rainy = True # дождь будет
+# if is_rainy:
+#     print('Беру зонт')
+#     print('Надеть сапоги')
+# else:
+#     print('не беру зонт')
+#
+# print('иду гулять')
+# else если False
+# is_rainy = false # дождя не будет
+# if is_rainy:
+#     print('беру зонт')
+#     print('одеваю сапоги')
+# else:
+#     print('не беру зонт')
+#
+# print('иду гулять')
+# if_ без else
+# if если True или False
+# is_rainy = True # дождь будет
+#
+# if is_rainy:
+#     print('беру зонт')
+# print('иду гулять')
+# is_rainy = False # дождя не будет
+# if is_rainy:  #: ничего распечатано не будет
+#     print('беру зонт')
+# print('иду гулять')
+# is_rainy = True # дождь будет
+# heavy_rain = False # несильный дождь
+# if is_rainy:
+#     if heavy_rain:
+#         print('беру зонт')
+#     else:
+#         print("беру дождевик")
+# else:
+#     print('не беру зонт')
+# print('иду гулять')
+#
+# is_rainy = True # дождь будет
+# heavy_rain = True # сильный дождь
+# if is_rainy:
+#     if heavy_rain:
+#         print('беру зонт')
+#     else:("беру дождевик")
+# else:
+#     print('не беру зонт')
+# print('иду гулять')
+# print("пять больше трех?", 5 > 3) # True
+# print("длина слова равно 1?", len("hello") == 1) # False
+# print("7 не равно 12?", 7 != 12) #True
+# list_ = [1, 2, 3, 4, 5]
+#
+# print("в списке есть число 7?", 7 in list_) #false
+# some_var = None
+# other_var = None
+#
+# print(some_var is other_var) #True
+# посчитаем пример
+# example = (2 > 3) and (2 < 2) or (1 != 5) and (not (5 < 3) or (3 == 1))
+#
+# my_result = False and False or True and (not False or False)
+# my_result = False and False or True and (True or False)
+# my_result = (False and False) or (True and True)
+# my_result = False or True
+# my_result = True
+#
+# print(example)
+# print(my_result)
+# print(example == my_result)
+# str_1 = 'test'
+# str_2 = "test"
+# str_3 = '''test'''
+# str_4 = """test"""
+#
+# print(str_1 == str_2 == str_3 == str_4)
+# print(
+#     (str_1 == str_2)
+#     and (str_2 == str_3)
+#     and (str_3 == str_4)
+# )
+# # Плохо
+# if a > 10:
+#     print('a больше 10')
+# else:
+#     if a < 10:
+#         print('a меньше 10')
+#     else:
+#         print('a равно 10')
+# # Хорошо
+# if a > 10:
+#     print('a больше 10')
+# elif a < 10:
+#     print('a меньше 10')
+# else:
+#     print('a равно 10')
