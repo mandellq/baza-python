024213dcd0c30870a1c5029128bd3b3fa7a12452
mandellq <167519564+mandellq@users.noreply.github.com>
2024-05-16 12:45:08 +0300

    Add files via upload

---
+++ task 7.py
@@ -0,0 +1,52 @@
+# spring_months = [3, 4, 5]
+# summer_months = [6, 7, 8,]
+# autumn_months = [9 ,10, 11]
+# winter_months = [1, 2, 12]
+# month = 15
+# if month in spring_months:
+#     print("Весна")
+# elif month in summer_months:
+#     print("Лето")
+# elif month in autumn_months:
+#     print("Осень")
+# elif month in winter_months:
+#     print("Зима")
+# else:
+#     print("Некорректный номер месяца")
+
+# is_logged_in = True
+# has_items_in_cart = True
+# has_shipping_address = True
+# def can_place_order():
+#     return is_logged_in and has_items_in_cart and has_shipping_address
+# total_purchase = 900
+# min_purchase = 1000
+# is_first_order = False
+# def can_get_discount():
+#     return total_purchase >= min_purchase and not is_first_order
+# if can_place_order():
+#     print("Можно оформить заказ.")
+# else:
+#     print("Нельзя оформить заказ.")
+# if can_get_discount():
+#     print("Можно получить скидку.")
+# else:
+#     print("Нельзя получить скидку.")
+
+#
+# number = int(input("Введите целое число: "))
+# def is_lucky_number(number):
+#     return number in [7, 13, 21]
+# def is_in_range(number):
+#     return 1 <= number <= 100
+# def check_number(number):
+#     if is_lucky_number(number):
+#         print("Счастливое число!")
+#     elif is_in_range(number):
+#         print("Число в указанном диапазоне")
+#     else:
+#         print("Не повезло")
+#
+# check_number(number)
+
+
